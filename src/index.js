9c19035c77ba51d087422931cd144d06c6eb1994
jondev <jonmac2454@gmail.com>
2025-03-16 18:14:33 +0300

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+let form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+ 
+  handleTodo(e.target.querySelector('#new-task-description').value)
+  form.reset()
+ })
+
 });
+
+function handleTodo(todo) {
+ let p = document.createElement('p')
+ let btn = document.createElement('button')
+ btn.addEventListener('click', doDelete)
+ btn.textContent = 'x'
+ p.textContent = `${todo}`
+ p.appendChild(btn)
+ document.querySelector('#list').appendChild(p)
+ 
+}
+function doDelete(e) {
+  e.target.parentNode.remove()
+}
